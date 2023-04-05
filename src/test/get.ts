ca2883ccff6410ab428d915bff7536e32af0620b
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-04-05 14:47:13 +0200

    fix page

--- src/test/get.ts
+++
@@ -1,8 +0,0 @@
-async function logJSONData() {
-    const response = await fetch("https://opendataspace.sos-ch-dk-2.exo.io/test/index.json");
-    const jsonData = await response.json();
-    console.log(jsonData);
-}
-
-logJSONData();
-  
--- src/test/put.ts
+++
@@ -1,9 +0,0 @@
-function createObject(path: any, data: any) {
-    const appS3Config = getAppS3Config();
-    const instanceS3 = new S3StorageService(appS3Config);
-    instanceS3.uploadObject(path, data);
-}
-
-const key = "test/username.json";
-const data = { username: "TEST" };
-createObject(key, data);
