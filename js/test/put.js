1fdea4a648a8083c279d6cd488302c4606b7754a
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-04-19 09:59:04 +0200

    remove app data

--- js/test/put.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function createObject(path, data) {
-    const appS3Config = getAppS3Config();
-    const instanceS3 = new S3StorageService(appS3Config);
-    instanceS3.uploadObject(path, data);
-}
-const key = "test/username.json";
-const data = { username: "TEST-Lokal" };
-createObject(key, data);
